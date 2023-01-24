16089ba6139f0642dbe475effb027fbba7b2130c
Elton Sanchez <3165000+sanchezelton@users.noreply.github.com>
2023-01-24 07:50:32 -0800

    - added hello Hello world for python lessons - removed appSettings.Development.json

---
+++ HelloPython/script.py
@@ -0,0 +1,14 @@
+# Lesson 1:
+# Hello world (https://www.learnpython.org/en/Hello%2C_World%21)
+# based on lessons available at www.learnpython.org
+
+# print function
+print("This line will be printed.")
+
+# simple conditional and indentation
+x = 1
+if x == 1:
+    print("x is 1")
+
+# hello world
+print("Hello, World!")
